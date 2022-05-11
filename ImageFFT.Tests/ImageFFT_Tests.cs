6fdd8a42091b06d4747fdaf50b5901d1d477b6ae
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-05-11 21:02:52 +0100

    - do a pixel compare o verify round trip

--- ImageFFT.Tests/ImageFFT_Tests.cs
+++ ImageFFT.Tests/ImageFFT_Tests.cs
@@ -1,19 +1,33 @@
 namespace ImageFFT.Tests;
 
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using FluentAssertions;
 using NUnit.Framework;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
 public class ImageFFT_Tests
 {
-    [Test]
-    public void RoundTrip_Succeeds()
-    {
-        var baseImg = Image.Load<Rgba32>("images/lena.png");
+  [Test]
+  public void RoundTrip_Succeeds()
+  {
+    var baseImg = Image.Load<Rgba32>("images/lena.png");
+    var baseBytes = new byte[baseImg.Width * baseImg.Height * Unsafe.SizeOf<Rgba32>()];
+    baseImg.CopyPixelDataTo(baseBytes);
 
-        var fft = FFT2D.Forward(baseImg);
+    var fft = FFT2D.Forward(baseImg);
 
-        var invImg = FFT2D.Inverse(fft);
-        invImg.SaveAsPng("InvImg.png");
-    }
+    var invImg = FFT2D.Inverse(fft);
+    var invBytes = new byte[invImg.Width * invImg.Height * Unsafe.SizeOf<Rgba32>()];
+    invImg.CopyPixelDataTo(invBytes);
+
+    invImg.Width.Should().Be(baseImg.Width);
+    invImg.Height.Should().Be(baseImg.Height);
+    invBytes.Length.Should().Be(baseBytes.Length);
+
+    // looks like round trip alters alpha (?), so compare within a tolerance
+    invBytes.Should().Equal(baseBytes, (left, right) => Math.Abs(left - right) <= 1);
+  }
 }
